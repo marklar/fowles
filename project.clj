e669506e72eae358065d489be734a978dc4d0281
Mark Wong-VanHaren <markwvh@gmail.com>
2014-06-17 17:56:36 +0200

    more fetcher

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 ;; [org.clojure/core.async "0.1.0-SNAPSHOT"]
-                 [http-kit "2.1.16"]
-                 [org.clojure/data.json "0.2.4"]
-                 [org.clojure/core.match "0.2.1"]
-                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [http-kit "2.1.16"]]
+                 ;; [org.clojure/data.json "0.2.4"]
+                 ;; [org.clojure/core.match "0.2.1"]
+                 ;; [org.clojure/tools.cli "0.3.1"]]
                  ;; [org.zeromq/cljzmq "0.1.5-SNAPSHOT"]]
-                 [com.keminglabs/zmq-async "0.1.0"]]
+                 ;; [com.keminglabs/zmq-async "0.1.0"]]
 
-  :main fowles.ping-pong
-  :aot [fowles.ping-pong]
+  :main fowles.fetcher
+  :aot [fowles.fetcher]
 
   :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
 
--- src/fowles/fetcher.clj
+++ src/fowles/fetcher.clj
@@ -1,16 +1,20 @@
 (ns fowles.fetcher
-  (:require [com.keminglabs.zmq-async.core :refer [register-socket!]]
+  (:refer-clojure :exclude [partition])
+  (:require ;; [com.keminglabs.zmq-async.core :refer [register-socket!]]
             [clojure.string :as str]
             [org.httpkit.client :as http]
-            [clojure.core.async :refer [alt! alts! timeout >! <! go chan
-                                        sliding-buffer close!]]))
+            [clojure.core.async
+             :refer [chan go partition map< split >! <!]
+             :as async]))
+
+;;-------------------------------------------
 
 ;; HTTP client
 
-;; Mark's personal API key
-;; AIzaSyBV8NYXYUl42UtckkRNov98i1mRrNxLJ_4
-
 (def api-key "AIzaSyBV8NYXYUl42UtckkRNov98i1mRrNxLJ_4")
+
+(defn csv [strs]
+  (str/join "," strs))
 
 (defn hmap->query-string [hmap]
   (->> hmap
@@ -18,28 +22,49 @@
        (map (fn [[k v]] (str k "=" v)))
        (str/join "&")))
 
-(defn csv [strs]
-  (str/join "," strs))
-
 ;; https://developers.google.com/youtube/v3/docs/videos/list?hl=ca
-(defn mk-video-url [api-key video-ids parts]
+(defn mk-video-uri [api-key video-ids parts]
   (let [args {:key api-key
               :id (csv video-ids)
               :part (csv parts)}]
     (str "https://www.googleapis.com/youtube/v3/videos?"
          (hmap->query-string args))))
 
-(def video-ids ["7lCDEYXw3mM"])
-(defn foo []
-  ;; start concurrent requests, get promise, half the waiting time
-  (let [parts ["snippet" "contentDetails" "statistics" "status"]
-        response1 (http/get (mk-video-url api-key video-ids parts))
-        response2 (http/get "http://clojure.org/")]
-    ;; Handle responses one-by-one, blocking as necessary
-    ;; Other keys :headers :body :error :opts
-    ;; (println "response1's status:" (:status @response1))
-    (println "response1:" @response1)))
-    ;; (println "response2's status:" (:status @response2))))
+
+;; Use `partition` to to create a channel
+;; which will create collections of items from another channel.
+;; Push individual video-ids onto A, partition them onto B.
+
+;; Channels
+;;   1. video-id   -> [video-id]                 (`partition`)
+;;   2. [video-id] -> uri                        (`map<`)
+;;   3. uri        -> response-promise           
+;;   4. response-promise -> good-response | bad-response   (`split`)
+
+
+(def IDS_PER_QUERY 2)
+(defn query-count [seq]
+  (/ (count seq) IDS_PER_QUERY))
+
+(defn mk-pipeline-chan
+  [api-key video-ids parts]
+  (->> (async/to-chan video-ids)
+       (async/partition IDS_PER_QUERY)
+       (async/map< #(mk-video-uri api-key % parts))
+       ;; http://http-kit.org/client.html
+       (async/map< #(deref (http/get %)))))
+
+;; this doesn't work
+;; async/close!))
+
+;; (doseq [vid-id video-ids]
+;; (go (async/>! ch vid-id)))
 
 (defn -main []
-  (foo))
+  (let [video-ids ["7lCDEYXw3mM" "MjtOzLfebgY" "6QIw1BQIvT4" "2xJWQPdG7jE"]
+        parts     ["snippet" "contentDetails" "statistics" "status"]
+        ch        (mk-pipeline-chan api-key video-ids parts)]
+    (go (dotimes [_ (query-count video-ids)]
+          (println (async/<! ch))))
+    (Thread/sleep 10000)
+))

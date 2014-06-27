4701e1a0efb671e2708d5c0e9249c3efc43e5532
Mark Wong-VanHaren <markwvh@gmail.com>
2014-06-27 12:38:42 +0200

    waits up to 2 seconds for more input IDs, then uses what it has (if any).

--- src/fowles/fetch/admitter.clj
+++ src/fowles/fetch/admitter.clj
@@ -3,18 +3,21 @@
    + Source: from wherever (zmq socket?).
    + Data:   video-ids, queries.
    + Output: Put for requester."
-  (:refer-clojure :exclude [partition])
   (:require [fowles
+             [util :as util]
              [admitter :as admitter]]
-            [clojure.core.async :refer [partition] :as async]))
+            [clojure.core.async :refer [chan]]))
 
 (def BUFFER_SIZE 1000)
+(def MAX_WAIT_MS 2000)
 
 (defn video-ids-from-puller
   ":: (str, int, int) -> chan"
   [host port num-per-request]
-  (let [single-id-ch (admitter/from-puller host port)]
-    (async/partition num-per-request
-                     single-id-ch
-                     BUFFER_SIZE)))
-  
+  (let [single-id-ch (admitter/from-puller host port)
+        out-ch (chan BUFFER_SIZE)]
+    (.start (Thread. #(util/pipe-groups-of-up-to-n single-id-ch
+                                                   out-ch
+                                                   num-per-request
+                                                   MAX_WAIT_MS)))
+    out-ch))
--- src/fowles/util.clj
+++ src/fowles/util.clj
@@ -2,10 +2,36 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [zeromq.zmq :as zmq]
+            [clojure.core.async :refer [>!! alt!! timeout]]
             [clj-time
              [core :as t]
              [coerce :as c]
              ]))
+
+;;-------------------------------
+;; async
+
+(defn- get-n-in-ms
+  "grab up n items from in-ch within max-ms."
+  [in-ch n max-ms]
+  (let [t (timeout max-ms)]
+    (loop [i 0, acc []]
+      (if (= i n)
+        acc
+        (alt!!
+         t     ([_] acc)
+         in-ch ([v] (recur (inc i) (conj acc v))))))))
+
+(defn pipe-groups-of-up-to-n
+  "grab up to num items from from in-ch and return as one msg on out-ch"
+  [in-ch out-ch num max-wait-ms]
+  (while true
+    (let [vs (get-n-in-ms in-ch num max-wait-ms)]
+      (if (> (count vs) 0)
+        (>!! out-ch vs)))))
+
+;;-------------------------------
+;; ZMQ
 
 (defn- mk-connect-addr
   [host port]
@@ -21,7 +47,8 @@
 (def mk-pusher (partial mk-socket :push))
 (def mk-puller (partial mk-socket :pull))
 
-;;-------------------------------  
+;;-------------------------------
+;; YouTube Requests
 
 (defn update-request-arg
   [request name val]
@@ -30,7 +57,7 @@
     {:query-type qt
      :args (assoc args name val)}))
 
-;;-------------------------------  
+;;-------------------------------
 
 ;; Why doesn't this work?
 (defn prep-shutdown []

20f9256a4965d15ad8693c4795d4d0ffebca5e65
Mark Wong-VanHaren <markwvh@gmail.com>
2014-07-08 21:28:37 +0200

    go-ified the puller (input)

--- src/fowles/admitter.clj
+++ src/fowles/admitter.clj
@@ -5,30 +5,35 @@
    + Output: Put for requester."
   (:require [fowles.util :as util]
             [zeromq.zmq :as zmq]
-            [clojure.core.async :refer [chan >!!]]))
+            [clojure.core.async :refer [go-loop chan >!]]))
 
 (defn- mk-puller
   [ctx addr]
   (doto (zmq/socket ctx :pull)
     (zmq/connect addr)))
 
-(defn- enq-from-puller
-  [host port to-ch]
-  (let [ctx  (zmq/context)
-        addr (util/mk-connect-addr host port)]
-    (with-open [puller (mk-puller ctx addr)]
-      (println "Ready to receive input from addr:" addr)
-      (while true
-        (let [msg (zmq/receive-str puller)]
-          (println "receiving:" msg)
-          ;; -blocking- put
-          (>!! to-ch msg))))))
+(defn- report-unused-input
+  [msg]
+  ;; TODO: Report to failure sink!
+  )
 
 ;;-------------------
 
 (defn from-puller
   ":: int -> chan"
   [host port]
-  (let [to-ch (chan)]  ;; no buffer!
-    (.start (Thread. #(enq-from-puller host port to-ch)))
+    ;; (.start (Thread. #(enq-from-puller host port to-ch)))
+  (let [to-ch  (chan)
+        ctx    (zmq/context)
+        addr   (util/mk-connect-addr host port)
+        puller (mk-puller ctx addr)]
+    (println "Ready to receive input from addr:" addr)
+    (go-loop []
+      (let [msg (zmq/receive-str puller)]
+        (println "receiving:" msg)
+        (if (>! to-ch msg)
+          (recur)
+          (do
+            (report-unused-input msg)
+            (zmq/close puller)))))
     to-ch))
